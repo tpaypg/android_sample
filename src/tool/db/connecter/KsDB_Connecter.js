49e47caa66ff34d7be776c26d89f9fbf75e563e3
IsKyou <86505646+IsKyou@users.noreply.github.com>
2022-04-20 17:53:54 +0900

    K

--- src/tool/db/connecter/KsDB_Connecter.js
+++ src/tool/db/connecter/KsDB_Connecter.js
@@ -1,13 +1,13 @@
 "use strict";
 var mysql = require('mysql2/promise'); // mysql 변수에 mysql 모듈을 할당 
 var KsDbConnecter = mysql.createPool({
-    // host : '127.0.0.1', //host객체 - 마리아DB가 존재하는 서버의 주소 
-    // user : 'root', //user객체 - 마리아DB의 계정 
-    // password : '0724', //password객체 - 마리아DB 계정의 비밀번호 
-    host: '203.228.206.141',
-    // host : '10.0.64.131', //host객체 - 마리아DB가 존재하는 서버의 주소 
-    user: 'msconn',
-    password: 'Jtnetqr12!',
+    host: '127.0.0.1',
+    user: 'root',
+    password: '0724',
+    // host : '203.228.206.141', //host객체 - 마리아DB가 존재하는 서버의 주소 
+    // // host : '10.0.64.131', //host객체 - 마리아DB가 존재하는 서버의 주소 
+    // user : 'msconn', //user객체 - 마리아DB의 계정 
+    // password : 'Jtnetqr12!', //password객체 - 마리아DB 계정의 비밀번호 
     database: 'msbase',
     waitForConnections: true,
     connectionLimit: 30,
--- src/tool/node/KsNode.ts
+++ src/tool/node/KsNode.ts
@@ -150,6 +150,7 @@ class KsNode {
     }
 
 
+
 }    
 
 export {KsNode};
